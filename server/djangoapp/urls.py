17b3d75da8ad2b632c6e89b643cff85e6636f2fd
Eric <nextstep@ericdee.me>
2022-01-16 23:36:49 -0700

    Finished week 1 requirements

---
+++ server/djangoapp/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'djangoapp'
+urlpatterns = [
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+
+    path(route='about/', view=views.about_page, name='about_us'),
+
+    path(route='contact/', view=views.contact_page, name='contact_us'),
+
+    # path for registration
+
+    # path for login
+
+    # path for logout
+
+    path(route='', view=views.get_dealerships, name='index'),
+
+    # path for dealer reviews view
+
+    # path for add a review view
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
