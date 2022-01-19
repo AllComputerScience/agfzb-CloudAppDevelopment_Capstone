c97e24abba4ec795c5dec3a6d87df73e4e3efe88
Eric <nextstep@ericdee.me>
2022-01-18 23:09:33 -0700

    Adding any missed files

--- functions/testing/python/main.py
+++ functions/testing/python/main.py
@@ -1,16 +1,9 @@
-#
-#
-# main() will be run when you invoke this action
-#
-# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-#
-# @return The output of this action, which must be a JSON object.
-#
-#
+# The documentation for "Cloudant" is not adequate.
+# As a result, all of the database functions reside in nodejs/index.js instead.
+
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 import requests
-
 
 def main(dict):
     databaseName = "dealerships"
